348bf6f8cd087f0b3239d11c278a5eeff34158c7
michelepiccinno <michele.piccinno@gmail.com>
2024-02-22 16:43:56 +0100

    add crud destroy

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -82,6 +82,7 @@ class EventController extends Controller
      */
     public function destroy(Event $event)
     {
-        //
+        $event -> delete();
+        return redirect()->route("admin.events.index");
     }
 }
--- resources/views/admin/events/show.blade.php
+++ resources/views/admin/events/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="card" style="width: 18rem;">
+    <div class="card mt-3" style="width: 20rem;">
         <div class="card-body">
             <h5 class="card-title">Nome: {{ $event->name }}</h5>
             <h6 class="card-subtitle mb-2 text-muted">Data Evento: {{ $event->date }}</h6>
@@ -13,6 +13,11 @@
             <p class="card-text">Creato da: {{ $event->user['name'] }}</p>
             <a href="{{ route('admin.events.index') }}" class="btn btn-secondary">Indietro</a>
             <a href="{{ route('admin.events.edit', $event->id) }}" class="btn btn-warning">Modifica</a>
+            <form action="{{ route('admin.events.destroy', $event->id) }}" method="POST" class="d-inline">
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="Elimina" class="btn btn-danger">
+            </form>
         </div>
     </div>
 @endsection
